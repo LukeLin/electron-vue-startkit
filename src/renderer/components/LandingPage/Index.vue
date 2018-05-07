e7f45ae0119eb00e4d74e4f01f5c92501ae2bcfd
LukeLin <773000785@qq.com>
2018-05-07 17:44:06 +0800

    update

---
+++ src/renderer/components/LandingPage/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="wrapper">
+    <main>
+        <div>test</div>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "landing-page",
+  methods: {
+  }
+});
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+    
+}
+
+</style>
